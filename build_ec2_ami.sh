61aa9581e9bb18a70f5913bab0d4cc29174112e9
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2017-08-20 22:48:56 +0200

    add pip install command for scr

--- build_ec2_ami.sh
+++ build_ec2_ami.sh
@@ -6,3 +6,5 @@ sudo gpasswd -a ec2-user docker
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+sudo pip-3.5 install simple-container-runtime
